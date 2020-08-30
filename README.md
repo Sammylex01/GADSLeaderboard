505ffa2080aa0d4eef1a78fd574eede6f80db136
Maxolex Togolais <maxolex12@gmail.com>
2020-08-30 17:01:14 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Google Africa Developer Scholarship Leaderboard Project (GADS 2020)
+## Screenshots
+<div style="display: flex">
+<img src="/Screenshots/0.png" width="300px"/>
+<img src="/Screenshots/1.png" width="300px"/>
+<img src="/Screenshots/2.png" width="300px"/>
+<img src="/Screenshots/3.png" width="300px"/>
+<img src="/Screenshots/4.png" width="300px"/>
+<img src="/Screenshots/5.png" width="300px"/>
+<img src="/Screenshots/6.png" width="300px"/>
+<img src="/Screenshots/7.png" width="300px"/>
+</div>
---
+++ app/src/main/java/com/maxolex/gadsleaderboard/viewmodel/SkillLeadersViewModel.java
@@ -0,0 +1,53 @@
+package com.maxolex.gadsleaderboard.viewmodel;
+
+import android.os.Handler;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.maxolex.gadsleaderboard.data.DataResponseCallback;
+import com.maxolex.gadsleaderboard.data.DataService;
+import com.maxolex.gadsleaderboard.model.LearningLeader;
+import com.maxolex.gadsleaderboard.model.SkillLeader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SkillLeadersViewModel extends ViewModel {
+    private MutableLiveData<List<SkillLeader>> skillLeaders;
+    private MutableLiveData<Boolean> error = new MutableLiveData<>(false);
+    private Handler handler = new Handler();
+
+    public LiveData<List<SkillLeader>> getSkillLeaders() {
+        if (skillLeaders == null) {
+            skillLeaders = new MutableLiveData<>();
+            refreshList();
+        }
+        return skillLeaders;
+    }
+
+    public LiveData<Boolean> getError() {
+        return error;
+    }
+
+    public void refreshList() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                DataService.getSkillLeaders(new DataResponseCallback<List<SkillLeader>>() {
+                    @Override
+                    public void onResponse(List<SkillLeader> response) {
+                        skillLeaders.setValue(response);
+                        error.setValue(false);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        error.setValue(true);
+                    }
+                });
+            }
+        }, 1000);
+    }
+}
